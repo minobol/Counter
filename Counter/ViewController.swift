c834862bff3fee1c4c82ef5f62bb2f93e09236ab
minobol <migelnokia@gmail.com>
2024-05-27 15:51:47 +0300

    Initial Commit

---
+++ Counter/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  Counter
+//
+//  Created by MacBook on 27.05.2024.
+//
+
+import UIKit
+import SwiftUI
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    override func printContent(_ sender: Any?) {
+        <#code#>
+        
+    }
+}
+
